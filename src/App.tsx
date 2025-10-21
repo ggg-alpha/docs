85d87090c0960d2509d13e7ff188618908a79937
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2025-10-21 18:25:03 +0900

    ホーム（インデックス）ページの内容をデフォルトのものに変換。

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
 import { Routes } from "react-router-dom";
 import "./index.css";
 
-import logo from "./logo.svg";
-import reactLogo from "./react.svg";
 import routes from "./Routes";
-
-// export function App() {
-//     return (
-//         <div className="app">
-//             <div className="logo-container">
-//                 <img src={logo} alt="Bun Logo" className="logo bun-logo" />
-//                 <img src={reactLogo} alt="React Logo" className="logo react-logo" />
-//             </div>
-
-//             <h1>Bun + React</h1>
-//             <p>
-//                 Edit <code>src/App.tsx</code> and save to test HMR
-//             </p>
-//         </div>
-//     );
-// }
 
 export function App() {
     return (
